37399fffd0fdb81acc0805911ee7192d0fd87aef
kychok98 <kychok98@gmail.com>
2024-09-03 22:36:34 +0800

    chore(frontend): ui add toast

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import { Toaster } from "@/lib/ui/toast";
 import LandingView from "@/views/LandingView.vue";
 </script>
 
 <template>
+  <Toaster />
   <LandingView />
 </template>
