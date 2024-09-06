c641367024fca03ad07347330ad9e8191505adcd
kychok98 <kychok98@gmail.com>
2024-09-07 01:19:19 +0800

    feat(frontend): vehicle add reset sort and column visibility

--- frontend/src/modules/Vehicle/DataTable/DataTableToolbar.vue
+++ frontend/src/modules/Vehicle/DataTable/DataTableToolbar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { Button } from "@/lib/ui/button";
-import { PlusIcon } from "@radix-icons/vue";
+import { PlusIcon, ResetIcon } from "@radix-icons/vue";
 import type { Table } from "@tanstack/vue-table";
 import { ref } from "vue";
 import DialogAdd from "../Dialog/DialogAdd.vue";
@@ -11,18 +11,30 @@ interface DataTableToolbarProps {
   table: Table<TVehicle>;
 }
 
-defineProps<DataTableToolbarProps>();
+const props = defineProps<DataTableToolbarProps>();
 
 const openAdd = ref(false);
+
+const handleReset = () => {
+  const { table } = props;
+  table.resetSorting();
+  table.resetColumnVisibility();
+};
 </script>
 
 <template>
-  <div class="flex items-center justify-end space-x-2">
-    <Button variant="outline" size="sm" class="h-8" @click="openAdd = true">
+  <div class="mt-2 flex items-center justify-between">
+    <Button variant="" size="sm" class="h-8" @click="openAdd = true">
       <PlusIcon class="mr-1 h-4 w-4" />
       Add
     </Button>
-    <DataTableViewOptions :table="table" />
+    <div class="space-x-2">
+      <Button variant="outline" size="sm" class="h-8" @click="handleReset">
+        <ResetIcon class="mr-1 h-4 w-4" />
+        Reset
+      </Button>
+      <DataTableViewOptions :table="table" />
+    </div>
   </div>
 
   <DialogAdd :open="openAdd" @update:open="(val: boolean) => (openAdd = val)" />
