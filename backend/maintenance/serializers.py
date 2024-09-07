acdc3bd29177b8d67ad126235b36ddc934fab988
kychok98 <kychok98@gmail.com>
2024-09-08 02:28:54 +0800

    chore(backend): maintenance serializer add create and vehicle

--- backend/maintenance/serializers.py
+++ backend/maintenance/serializers.py
@@ -5,18 +5,29 @@ from .models import Maintenance
 
 
 class MaintenanceSerializer(serializers.ModelSerializer):
-    vehicle_id = serializers.PrimaryKeyRelatedField(read_only=True)
+    vehicle_id = serializers.IntegerField(write_only=True)
+    vehicle = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Maintenance
-        fields = ['id', 'schedule_date', 'completion_date', 'vehicle_id', 'description', 'schedule_type']
-        read_only_fields = ['id']
+        fields = ['id', 'schedule_date', 'completion_date', 'vehicle', 'vehicle_id', 'description', 'schedule_type']
+        read_only_fields = ['id', 'vehicle']
 
     def validate(self, data):
         vehicle = data.get('vehicle')
 
-        # Ensure vehicle status is "Pending" before allowing creation
-        if vehicle and vehicle.status != 'Pending':
-            raise serializers.ValidationError('Only vehicles with status "Pending" can create maintenance records.')
+        # Ensure vehicle status is "Inactive" before allowing creation
+        if vehicle and vehicle.status != 'Inactive':
+            raise serializers.ValidationError('Only vehicles with status "Inactive" can create maintenance records.')
 
         return data
+
+    def create(self, validated_data):
+        vehicle = Maintenance.objects.create(
+            vehicle_id=validated_data['vehicle_id'],
+            schedule_date=validated_data['schedule_date'],
+            description=validated_data['description'],
+            schedule_type="manual",
+            completion_date=None,
+        )
+        return vehicle
