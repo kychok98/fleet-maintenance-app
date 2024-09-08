8b21cb24180f06adafffd7db68f0e706497d15a0
kychok98 <kychok98@gmail.com>
2024-09-08 17:30:38 +0800

    chore(frontend): remove unused dialogview

--- frontend/src/modules/Maintenance/components/DataTableRowActions.vue
+++ frontend/src/modules/Maintenance/components/DataTableRowActions.vue
@@ -12,7 +12,6 @@ import type { Row } from "@tanstack/vue-table";
 import { ref } from "vue";
 import DialogDelete from "../Dialog/DialogDelete.vue";
 import DialogEdit from "../Dialog/DialogEdit.vue";
-import DialogView from "../Dialog/DialogView.vue";
 import type { TMaintenance } from "../services/schema.ts";
 
 interface DataTableRowActionsProps {
@@ -20,7 +19,6 @@ interface DataTableRowActionsProps {
 }
 defineProps<DataTableRowActionsProps>();
 
-const openView = ref(false);
 const openDelete = ref(false);
 const openEdit = ref(false);
 </script>
@@ -37,18 +35,11 @@ const openEdit = ref(false);
       </Button>
     </DropdownMenuTrigger>
     <DropdownMenuContent align="end" class="w-[160px]">
-      <DropdownMenuItem @click="openView = true">View Details</DropdownMenuItem>
       <DropdownMenuItem @click="openEdit = true">Edit</DropdownMenuItem>
       <DropdownMenuItem @click="openDelete = true">Delete</DropdownMenuItem>
     </DropdownMenuContent>
   </DropdownMenu>
 
-  <DialogView
-    v-if="openView"
-    :row="row"
-    open
-    @update:open="(val: boolean) => (openView = val)"
-  />
   <DialogEdit
     v-if="openEdit"
     :row="row"
