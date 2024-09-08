49970f63a4c1497ca9cc257b5b6db2454d2630b8
kychok98 <kychok98@gmail.com>
2024-09-08 11:48:11 +0800

    chore(frontend): lib ui add popover, calendar, datepicker

--- frontend/src/lib/ui/select/SelectTrigger.vue
+++ frontend/src/lib/ui/select/SelectTrigger.vue
@@ -27,7 +27,7 @@ const forwardedProps = useForwardProps(delegatedProps);
     v-bind="forwardedProps"
     :class="
       cn(
-        'flex h-10 w-full items-center justify-between rounded-md border border-slate-200 bg-white px-3 py-2 text-start text-sm ring-offset-white placeholder:text-slate-500 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus:ring-slate-300 [&>span]:truncate',
+        'flex h-10 w-full items-center justify-between rounded-md border border-slate-200 bg-white px-3 py-2 text-start text-sm ring-offset-white placeholder:text-slate-500 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 [&>span]:truncate',
         props.class,
       )
     "
